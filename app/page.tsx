5ec26a48c128e0d6efab51344e6f9490398b5205
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-12-02 13:45:22 +0200

    create Event List section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-
+import EventList from "@/components/EventList";
 
 export default function Home() {
   return (
     <div className="">
-      <h1>Wellcome to Ticket MarketPlace</h1>
+      <EventList />
     </div>
   );
 }
